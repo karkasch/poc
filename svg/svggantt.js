5368027a4afbd49fdf78262df94e312c8871f530
Arkady Turchenko <arkady.turchenko@aerdata.com>
2016-06-21 17:58:40 +0200

    render

--- svg/svggantt.js
+++ svg/svggantt.js
@@ -6,10 +6,15 @@ class SvgGanttItem {
 
 class SvgGantt {
 
-    constructor(selector) {
-        this.selector = selector;
-        this.view = "month";
+    constructor(data) {
+        this.selector = data.selector;
+        this.view = data.view || "month";
         this.data = [];
+
+        var d = new Date();
+        this.startDate = new Date(d.getFullYear(), d.getMonth() - 3, 1);
+
+        console.log("this", this);
     }
 
     setView(view) {
@@ -19,33 +24,75 @@ class SvgGantt {
     addItem(data, show) {
         var line = null;
         for (var i = 0; i < this.data.length; i++) {
-            if (this.data[i].title == data.title) {
+            if (this.data[i].label == data.label) {
                 line = this.data[i];
                 break;
             }
         }
-        if (line == null)
-            line = { title: data.title, items: [] };
+        if (line == null) {
+            line = { label: data.label, items: [] };
+            this.data.push(line);
+        }
 
-        if (show)
-            this.showItem();
+        line.items.push(data);        
     }
 
     createItem(data) {
         return {
-            x: 0,
-            y: 0,
-            from: data.from,
-            to: data.to,
+            x: data.x,
+            y: data.y,
+            startDate: data.startDate,
+            endDate: data.endDate,
             label: data.label
         }
+    }
+
+    getX(date) {
+
     }
 
     showItem(item) {
 
     }
+
+    render() {
+        $(this.selector).empty();
+
+        var html = "";        
+        for (var i = 0; i < this.data.length; i++) {
+            var line = this.data[i];
+            for (var j = 0; j < line.items.length; j++) {
+                var g = line.items[j];
+                html += `<svg x="${g.x}" y="${(i * 50)}">
+	<rect width="100" height="30"></rect>
+    <text x="10" y="20" fill="#ffffff" stroke-width="0.2" font-size="13px">Shop visit</text>
+</svg>`;
+            }
+        }
+
+        $(this.selector).html(html);
+    }
 }
 
-var sgantt = new SvgGantt(".chart");
+var sgantt = new SvgGantt({ selector: ".chart" });
 
-console.log(sgantt.test());
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 1", x: i * 55, y: 30 });
+} 
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 2", x: i * 55, y: 30 });
+}
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 3", x: i * 55, y: 30 });
+}
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 4", x: i * 55, y: 30 });
+}
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 5", x: i * 55, y: 30 });
+}
+for (var i = 0; i < 1500; i += 2) {
+    sgantt.addItem({ label: "Eng 6", x: i * 55, y: 30 });
+}
+
+sgantt.render();
